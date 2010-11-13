3fea55d75b8a3895fd27fbfc7a2d16b88619c267
Harrison Strowd <h.strowd@gmail.com>
2010-11-13 09:31:39 +0000

    Added a utility to clean up my temp files, logs, and unit test results.

--- plugins/cnuapp-utils/cnuapp-utils.el
+++ plugins/cnuapp-utils/cnuapp-utils.el
@@ -1,5 +1,9 @@
 ;; ------------- CnuApp Utilities -----------------
 
+(setq cnuapp-log-dir "/var/log/cnuapp/")
+(setq log-sub-dirs '(services.d space.d lsws.d))
+(setq cnuapp-dir "/export/web/cnuapp/")
+
 (defun cnu-change-env (country)
   "Changes the symlink for the /etc/cnu/cnu_env file to point
 to the requested country"
@@ -10,6 +14,7 @@ to the requested country"
 	  (call-process "ln" nil "*Message*" nil "-fs" country-file "/etc/cnu/cnu_env")
 	  (message "Success: Changed environment to %s" country-file))
       (error "Failed: File '%s' does not exist." country-file))))
+
 
 (defun cnu-app-restart ()
   "Restarts the cnuapp services."
@@ -27,7 +32,7 @@ to the requested country"
   "Stops the cnuapp services."
   (interactive)
   ; This uses TRAMP to run the sudo command. I'm not really sure how it works. 
-  ; I found it on a google group post.
+  ; I found it on a google group post and it appears to work.
   (with-temp-buffer
     (cd "/sudo::/")
     (let ((result (shell-command-to-string "sudo sv d /var/service/*")))
@@ -46,6 +51,86 @@ to the requested country"
       (if (= 0 (length result))
 	  (message "Success: The app is now being started")
 	(error "Failed: Unable to restart the app. Error: %s" result)))))
+
+
+(defun cnu-auto-clean-house ()
+  "Invokes the function to clean all cnuapp garbage."
+  (interactive)
+  (cnu-clean-house))
+
+(defun cnu-clean-house (&optional skip-p4-tmp skip-logs skip-test-results)
+  "Deletes temporary files, cnuapp logs, and unit test results."
+  (interactive "MSkip p4 temporary files(y/n)? \nMSkip cnuapp logs(y/n)? \nMSkip-test-results")
+  (if (or (not skip-p4-tmp) 
+	  (equal (downcase skip-p4-tmp) "false")
+	  (equal (downcase skip-p4-tmp) "f")
+	  (equal (downcase skip-p4-tmp) "no")
+	  (equal (downcase skip-p4-tmp) "n"))
+      (kill-p4-temp-files))
+  (if (or (not skip-logs)
+	  (equal (downcase skip-p4-tmp) "false")
+	  (equal (downcase skip-p4-tmp) "f")
+	  (equal (downcase skip-p4-tmp) "no")
+	  (equal (downcase skip-p4-tmp) "n"))
+      (kill-cnuapp-logs))
+  (if (or (not skip-test-results)
+	  (equal (downcase skip-p4-tmp) "false")
+	  (equal (downcase skip-p4-tmp) "f")
+	  (equal (downcase skip-p4-tmp) "no")
+	  (equal (downcase skip-p4-tmp) "n"))
+      (kill-cnuapp-unit-test-results)))
+
+(defun kill-p4-temp-files ()
+  "Deletes the following p4 temporary files, if found:
+  - branch backups
+  - changelist specs"
+  (interactive)
+
+  (if p4-chglst-file-prefix
+      (let ((rm-result (shell-command-to-string (format "rm %s*" p4-chglst-file-prefix))))
+	(if (rm-failed? rm-result) 
+	    (error "Failed: Unable to remove p4 temporary files from %s. Error: %s"
+		   p4-chglst-file-prefix rm-result))))
+
+  (if p4-branch-backup-dir
+      (let ((rm-result (shell-command-to-string (format "rm -r %s*" p4-branch-backup-dir))))
+	(if (rm-failed? rm-result) 
+	    (error "Failed: Unable to remove p4 branch backups from %s. Error: %s" 
+		   p4-branch-backup-dir rm-result)))))
+
+(defun kill-cnuapp-logs ()
+  "Deletes all cnuapp logs. Including all logs from the following sub-directories:
+  - services.d
+  - space.d
+  - lsws.d"
+  (interactive)
+  (let ((rm-result (shell-command-to-string (format "rm %s*" cnuapp-log-dir))))
+    (if (rm-failed? rm-result) 
+	(error "Failed: Unable to clear cnuapp log directory: %s. Error: %s" 
+	       cnuapp-log-dir rm-result)))
+
+; TODO: Should I be removing these files?
+;  (dolist (sub-dir log-sub-dirs)
+;    (let ((rm-result (shell-command-to-string (format "rm %s%s/@4*" cnuapp-log-dir sub-dir))))
+;      (if (rm-failed? rm-result) 
+;	  (error "Failed: Unable to clear cnuapp log directory: %s%s. Error: %s" 
+;		 cnuapp-log-dir sub-dir rm-result))))
+  )
+
+(defun kill-cnuapp-unit-test-results ()
+  "Deletes all cnuapp unit test results."
+  (let ((rm-result (shell-command-to-string (format "rm -r %sresult*" cnuapp-dir))))
+    (if (rm-failed? rm-result) 
+	(error "Failed: Unable to clear cnuapp unit test results from %s. Error: %s" 
+	       cnuapp-dir rm-result))))
+
+(defun rm-failed? (rm-result)
+  "Checks if the provide string indicates a rm failure."
+  (dolist (result-line (split-string rm-result "\n"))
+    (if (and (> (length result-line) 0) 
+	     (not (string-match ": No such file or directory" result-line))
+	     (not (string-match ": Is a directory" result-line)))
+	(return t))))
 
 
 (provide 'cnuapp-utils)
